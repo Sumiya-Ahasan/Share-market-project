fcaa237d7cbcddbd442b93a5a4063db086983a33
Sumiya Ahasan <sumiya5228ahasan@gmail.com>
2025-10-14 22:40:53 +0600

    Update WebPageConnection.py

--- WebPageConnection.py
+++ WebPageConnection.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 st.title("Hello! Share market")
